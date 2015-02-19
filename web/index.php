dc3ab4c2be6135b6f77721b62ab2217f3c2fa051
mickey <mikio.nozaki@gmail.com>
2015-02-20 08:10:21 +0900

    connect test

--- web/index.php
+++ web/index.php
@@ -1,34 +1,5 @@
 <?php
-echo "hello!";
-// phpinfo();
-// exit;
- $mongo_url = parse_url(getenv("MONGO_URL"));
-$dbname = str_replace("/", "", $mongo_url["path"]);
 
- # connect
-$m   = new MongoClient(getenv("MONGO_URL"));
-$db  = $m->$dbname;
-$col = $db->access;
-
- # print all existing documents
-  $data = $col->find();
-  foreach($data as $visit) {
-    echo "<li>" . $visit["ip"] . "</li>";
-  }
-
-  # disconnect
-  $m->close();
-
-
-
-
-
-
-
-
-
-
-exit;
 $connection = new Mongo(getenv('MONGOLAB_URI'));
 $db = $connection->selectDB(getenv('MONGOLAB_DB1'));
 $col = $db->selectCollection(getenv('MONGOLAB_COL1'));
